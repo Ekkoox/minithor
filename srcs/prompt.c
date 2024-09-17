1f35e59f4941c8320e5964862ae680ac543007b3
Enzo Schneider <enschnei@made-f0Cr9s5.clusters.42paris.fr>
2024-09-17 18:25:50 +0200

    prompt done and fix

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,33 +6,62 @@
 /*   By: enschnei <enschnei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 21:43:01 by enschnei          #+#    #+#             */
-/*   Updated: 2024/09/16 22:17:01 by enschnei         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:19:24 by enschnei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	error_prompt(char *buffer, ssize_t bytes_read)
+{
+	if (!buffer)
+	{
+		perror("Error calloc");
+		exit(EXIT_FAILURE);
+	}
+	if (bytes_read < 0)
+	{
+		perror("Error reading input");
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	exit_prompt(char *buffer)
+{
+	if (ft_strcmp(buffer, "exit") == 0)
+	{
+		ft_putstr_fd("Bisous mon chou <3\n", 1);
+		return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
+
 int	creat_the_prompt(void)
 {
-	char *buffer;
-	size_t buf_size;
+	char	*buffer;
+	size_t	buf_size;
+	ssize_t	bytes_read;
 
 	buf_size = BUFFER_SIZE;
-
-	// alloc buffer qui stockera la commande entree par l'user
+	bytes_read = 0;
 	buffer = (char *)ft_calloc(sizeof(char), buf_size);
-	if (buffer == NULL)
-	{
-		perror("Malloc failure");
-		return (EXIT_FAILURE);
-	}
+	if (!buffer)
+		error_prompt(buffer, bytes_read);
 	ft_putstr_fd(">", 1);
-	// lecture de STDIN en boucle
-	while (getline(&buffer, &buf_size, stdin) > 0)
+	while ((bytes_read = read(STDIN_FILENO, buffer, buf_size - 1)) > 0)
 	{
+		buffer[bytes_read] = '\0';
+		if (buffer[bytes_read - 1] == '\n')
+			buffer[bytes_read - 1] = '\0';
+		if (exit_prompt(buffer) == 0)
+			break ;
 		ft_printf("cmd = %s\n", buffer);
 		ft_putstr_fd(">", 1);
 	}
+	if (bytes_read < 0)
+		error_prompt(buffer, bytes_read);
 	free(buffer);
 	return (EXIT_SUCCESS);
 }
